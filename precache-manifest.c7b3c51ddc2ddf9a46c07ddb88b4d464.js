e4961527ef8f5d7b555d248bfbda74ac3fd914b5
congthanh-tk <ma.cong.thanh@mqsolutions.com.vn>
2020-06-15 22:38:22 +0700

    Updates

---
+++ precache-manifest.c7b3c51ddc2ddf9a46c07ddb88b4d464.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c89c7a6d3bad85d58f06eda5e794da4",
+    "url": "/react-face-recognition/index.html"
+  },
+  {
+    "revision": "7c0aa06847e8d096ad0c",
+    "url": "/react-face-recognition/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "58682b13e505b514b4fd",
+    "url": "/react-face-recognition/static/js/2.79d6913d.chunk.js"
+  },
+  {
+    "revision": "79e49d9635a2f3299cb519bb46bd11c3",
+    "url": "/react-face-recognition/static/js/2.79d6913d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c0aa06847e8d096ad0c",
+    "url": "/react-face-recognition/static/js/main.4d73e3f1.chunk.js"
+  },
+  {
+    "revision": "ce1fdfbb7952e0e6e3f9",
+    "url": "/react-face-recognition/static/js/runtime-main.3add8129.js"
+  },
+  {
+    "revision": "ccd20247dfe4bef3cae08bbc4eeede84",
+    "url": "/react-face-recognition/static/media/test.ccd20247.jpg"
+  }
+]);
